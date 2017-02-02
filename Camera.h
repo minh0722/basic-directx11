5f787a1c71c7a9448bc5edd3af2a7ea3fda2e155
Minh Nguyen <minh0722@yahoo.com>
2017-02-03 00:47:04 +0200

    added forgotten header

--- Camera.h
+++ Camera.h
@@ -7,6 +7,8 @@ public:
     void SetTranslation(const Vector4f& translation);
     void SetRotation(Axis axis, float degree);
     void SetScale(const Vector4f& scale);
+
+	Matrix44f GetViewMatrix();
 private:
     Matrix44f m_WorldMatrix;
 
