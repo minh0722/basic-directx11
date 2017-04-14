b9f9c64a988c3ac23ed5ea881c36a2d01252c5d1
Minh Nguyen <minh0722@yahoo.com>
2017-04-14 11:30:11 +0300

    updated scrip to copy files to the separate folder

--- copyFiles.sh
+++ copyFiles.sh
@@ -16,10 +16,10 @@ elif [ "$1" == "--vstogit" ]; then
 	cp "${FOLDER}"/*.h .
 	cp "${FOLDER}"/*.cpp .
 	cp "${FOLDER}"/*.hlsl .
-	cp "${FOLDER}"/../dxProject.sln .
-	cp "${FOLDER}"/dxProject.vcxproj .
-	cp "${FOLDER}"/dxProject.vcxproj.filters .
-	cp "${FOLDER}"/dxProject.vcxproj.user .
+	cp "${FOLDER}"/../dxProject.sln ./VS2015
+	cp "${FOLDER}"/dxProject.vcxproj ./VS2015
+	cp "${FOLDER}"/dxProject.vcxproj.filters ./VS2015
+	cp "${FOLDER}"/dxProject.vcxproj.user ./VS2015
 else
 	echo "Invalid argument was given"
 	exit 0
