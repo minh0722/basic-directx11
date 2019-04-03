6dc6ebbdab8b7d614e7eeb30150fcf402a02c4f6
Minh Nguyen <minh0722@yahoo.com>
2019-04-03 18:28:04 +0300

    spaceshipp renders ugly colors for now

--- SpaceshipVertexShader.hlsl
+++ SpaceshipVertexShader.hlsl
@@ -18,15 +18,24 @@ struct PixelOutput
 	float4 color : COLOR;
 };
 
-PixelOutput main(VertexInput inputVertex)
+PixelOutput main(VertexInput inputVertex, uint vertexId : SV_VertexID)
 {
 	PixelOutput output;
 
 	output.pos = mul(world, float4(inputVertex.pos, 1.0f));
 	output.pos = mul(view, output.pos);
 	output.pos = mul(proj, output.pos);
+    
+    // 0, 3 -> (1, 0, 0)
+    // 1, 4 -> (0, 1, 0)
+    // 2, 5 -> (0, 0, 1)
+    uint remainder = vertexId % 3;
+    if (remainder == 0)
+        output.color = float4(1.0f, 0.0f, 0.0f, 1.0f);
+    else if (remainder == 1)
+        output.color = float4(0.0f, 1.0f, 0.0f, 1.0f);
+    else
+        output.color = float4(0.0f, 0.0f, 1.0f, 1.0f);
 
-	output.color = float4(0.0f, 1.0f, 0.0f, 1.0f);
-
-	return output;
+    return output;
 }
