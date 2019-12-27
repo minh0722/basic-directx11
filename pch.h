092adf81e28ca5eab718d737fdd26afee8dc197a
minh0722 <minh0722@yahoo.com>
2019-12-27 23:43:48 +0200

    add aliasing for some integer types

--- pch.h
+++ pch.h
@@ -31,6 +31,15 @@
 
 #include "common.h"
 
+using u8 = uint8_t;
+using u16 = uint16_t;
+using u32 = uint32_t;
+using u64 = uint64_t;
+using i8 = int8_t;
+using i16 = int16_t;
+using i32 = int32_t;
+using i64 = int64_t;
+
 static const UINT screenWidth = 1080;
 static const UINT screenHeight = 720;
 
