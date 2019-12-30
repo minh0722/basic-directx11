f7ac7939f6f0d0ac8c2d3fba4467bb1509c5da49
minh0722 <minh0722@yahoo.com>
2019-12-30 23:59:31 +0200

    remove the testing in vs for spaceship

--- SpaceshipVertexShader.hlsl
+++ SpaceshipVertexShader.hlsl
@@ -27,17 +27,6 @@ VertexOutput main(VertexInput inputVertex, uint vertexId : SV_VertexID)
 	output.pos = mul(world, float4(inputVertex.pos, 1.0f));
 	output.pos = mul(view, output.pos);
 	output.pos = mul(proj, output.pos);
-    
-    // 0, 3 -> (1, 0, 0)
-    // 1, 4 -> (0, 1, 0)
-    // 2, 5 -> (0, 0, 1)
-    uint remainder = vertexId % 3;
-    if (remainder == 0)
-        output.color = float4(1.0f, 0.0f, 0.0f, 1.0f);
-    else if (remainder == 1)
-        output.color = float4(0.0f, 1.0f, 0.0f, 1.0f);
-    else
-        output.color = float4(0.0f, 0.0f, 1.0f, 1.0f);
 
     output.uv = inputVertex.uv;
 
