68dabc2b4c70f5e991e584236dd7aeb3567f1802
minh0722 <minh0722@yahoo.com>
2019-12-31 11:43:35 +0200

    remove vertex color output for baker too

--- impostorBakerVertexShader.hlsl
+++ impostorBakerVertexShader.hlsl
@@ -16,7 +16,6 @@ struct VertexOutput
 {
     float4 pos : SV_POSITION;
     float2 uv : TEXCOORD0;
-    float4 color : COLOR;
 };
 
 VertexOutput main(VertexInput inputVertex, uint vertexId : SV_VertexID)
@@ -25,17 +24,6 @@ VertexOutput main(VertexInput inputVertex, uint vertexId : SV_VertexID)
 
     output.pos = mul(view, inputVertex.pos);
     output.pos = mul(proj, output.pos);
-
-    // 0, 3 -> (1, 0, 0)
-    // 1, 4 -> (0, 1, 0)
-    // 2, 5 -> (0, 0, 1)
-    uint remainder = vertexId % 3;
-    if (remainder == 0)
-        output.color = float4(1.0f, 0.0f, 0.0f, 1.0f);
-    else if (remainder == 1)
-        output.color = float4(0.0f, 1.0f, 0.0f, 1.0f);
-    else
-        output.color = float4(0.0f, 0.0f, 1.0f, 1.0f);
 
     output.uv = inputVertex.uv;
 
