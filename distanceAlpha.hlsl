ed11247a4424442a3c177f6be9ba891b364c2168
minh0722 <minh0722@yahoo.com>
2020-01-11 01:19:39 +0200

    add standard stuff for indexing in cs

--- distanceAlpha.hlsl
+++ distanceAlpha.hlsl
@@ -1,7 +1,16 @@
-
+#include "computeShadersCommon.h"
 
 [numthreads(1, 1, 1)]
 void main( uint3 DTid : SV_DispatchThreadID )
 {
+    uint idx = id.x + id.y * MAX_DIM_THREADS;
+
+    uint width;
+    uint height;
+    source.GetDimensions(width, height);
+
+    uint x = idx % height;
+    uint y = (idx - x) / width;
+
 
 }
