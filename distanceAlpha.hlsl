10f8f5772218de0f2c2c767d291745ec1f8c00ff
minh0722 <minh0722@yahoo.com>
2020-01-11 20:24:52 +0200

    fix shader build

--- distanceAlpha.hlsl
+++ distanceAlpha.hlsl
@@ -1,7 +1,9 @@
 #include "computeShadersCommon.h"
 
+Texture2D<float4> source : register(t0);
+
 [numthreads(1, 1, 1)]
-void main( uint3 DTid : SV_DispatchThreadID )
+void main( uint3 id : SV_DispatchThreadID )
 {
     uint idx = id.x + id.y * MAX_DIM_THREADS;
 
