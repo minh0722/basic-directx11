245d9b32ea0c6af980f804dfea2ebba422c7ac29
Minh Nguyen <minh0722@yahoo.com>
2020-01-30 17:19:42 +0200

    ImpostorSample added to ps

--- renderer.cpp
+++ renderer.cpp
@@ -111,7 +111,7 @@ void Renderer::Render(InputClass* input)
 
     m_DebugDisplay->Render(this);
     
-	static bool baked = false;
+	static bool baked = true;
 	if (!baked)
 	{
         RENDERDOC_BEGIN_CAPTURE
