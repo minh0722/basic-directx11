dec954bef70c0d8abd3915230daee36c3c6c7f75
Minh Nguyen <minh0722@yahoo.com>
2020-08-28 01:03:00 +0300

    add ambient lighting for spaceship, constant 0.1f for now

--- SpaceshipPixelShader.hlsl
+++ SpaceshipPixelShader.hlsl
@@ -9,7 +9,7 @@ struct InputPixel
 cbuffer MaterialBuffer : register(b0)
 {
     float4 ambient;
-    float4 diffuse;
+    float4 diffuseColor;
     float4 specular;
 }
 
@@ -24,13 +24,19 @@ SamplerState wrapSampler : register(s0);
 
 float4 main(InputPixel inputPixel) : SV_TARGET
 {
+    // calculating ambient lighting
+    float ambientStrength = 0.1f;
+    float3 ambient = ambientStrength * diffuseColor;
+    
+    // calculating diffuse lighting
     float3 normal = normalize(inputPixel.normal);
     float3 lightDir = normalize(lightSourcePos.xyz - inputPixel.worldPos);
 
     float diff = max(dot(normal, lightDir), 0.0f);
-    float3 diffuseColor = diff * lightColor;
+    float3 diffuse = diff * lightColor;
 
-    float3 finalColor = diffuseColor * diffuse;
+    float3 finalColor = (diffuse + ambient) * diffuseColor.rgb;
+    
 
     return float4(finalColor, 1.0f);
 }
